a0dd4e97aaad8dc6f4ef90b692c5f35385d87fec
= <=>
2019-11-17 03:32:42 +0700

    im a mess

---
+++ src/TriangleTest.java
@@ -0,0 +1,8 @@
+public class TriangleTest {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        System.out.println(triangle);
+        triangle = new Triangle(4,2,5);
+        System.out.println(triangle);
+    }
+}
